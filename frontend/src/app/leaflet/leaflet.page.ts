4858e0d4c32add14a3e73c2d4be4d36501da6f60
Aripin2401 <ujangarifin06@gmail.com>
2022-10-08 21:31:01 +0700

    1. Leaflet Peta Terinstall

---
+++ frontend/src/app/leaflet/leaflet.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+@Component({
+  selector: 'app-leaflet',
+  templateUrl: './leaflet.page.html',
+  styleUrls: ['./leaflet.page.scss'],
+})
+export class LeafletPage {
+  map: L.Map;
+  constructor() { }
+
+  ionViewDidEnter() {
+    this.map = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
+      maxZoom: 18,
+    }).addTo(this.map);
+  }
+
+}
