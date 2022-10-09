e49588c8cdfc0f8f28e02005e6a3584bb35461ea
Aripin2401 <ujangarifin06@gmail.com>
2022-10-09 11:29:58 +0700

    2. Geocoder Search, Routing Machine  Terinstall

--- frontend/src/app/leaflet/leaflet.page.ts
+++ frontend/src/app/leaflet/leaflet.page.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import * as L from 'leaflet';
+import 'leaflet-routing-machine';
+import 'leaflet-sidebar-v2';
+import {Geocoder, geocoders} from 'leaflet-control-geocoder';
+
 @Component({
   selector: 'app-leaflet',
   templateUrl: './leaflet.page.html',
@@ -10,10 +14,30 @@ export class LeafletPage {
   constructor() { }
 
   ionViewDidEnter() {
-    this.map = L.map('map').setView([51.505, -0.09], 13);
+    this.map = L.map('map').setView([-6.934262277308666, 107.62608039631637], 13);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
       maxZoom: 18,
+    }).addTo(this.map);
+
+    // const GeocoderControl = new Geocoder();
+    // GeocoderControl.addTo(this.map);
+    // GeocoderControl.on('markgeocode', function (e) {
+    // console.log(e);
+    // });
+
+      L.Routing.control({ 
+        //Adding waypoints
+        waypoints: [
+          L.latLng(-6.934262277308666, 107.62608039631637),
+          L.latLng(-6.934262277308666, 107.62608039631637)
+        ],
+        //Adding a route planner
+        routeWhileDragging: true,
+        //Adding a route summary
+        show: true,
+        //Adding a sidebar
+        collapsible: true,        
     }).addTo(this.map);
   }
 
